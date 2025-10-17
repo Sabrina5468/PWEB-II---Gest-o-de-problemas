85dcda329a0b2a6ee6c2fb34c9400935ccb661e1
Sabrina5468 <ssf13@aluno.ifal.edu.br>
2025-10-17 14:03:55 -0300

    Delete src directory

--- src/service/IGestaoProblemas.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public interface IGestaoProblemas {
-    void cadastrarTecnico();
-    void registrarProblema();
-    void adicionarAcao();
-    void encerrarProblema();
-    void listarProblemas();
-}
